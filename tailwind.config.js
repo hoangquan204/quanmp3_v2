38cd8c5152e62fa446f25b1f4539a9676ac6c7b6
hoangquan204 <quanpogba888@gmail.com>
2024-12-28 13:20:55 +0700

    up

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#198754',
+        secondary: '#333'
+      }
+    },
+  },
+  plugins: [],
+}
+
